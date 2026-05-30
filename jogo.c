2571633dd08b5d8d9c4d30293dbe4b887ec06fbb
Arthur Henrique <123035993+Kuromyuto@users.noreply.github.com>
2026-05-30 14:04:31 +0000

    Implementando as peças

--- jogo.c
+++ jogo.c
@@ -3,7 +3,8 @@
 int main(){
     
     //VARIAVEIS DO JOGO
-    int decisao, decisao2, rei, bispo, torre;
+    int decisao, rei, bispo, torre, i;
+    char decisao2;
     
     //ENTRADA DO JOGO
     printf("## XADREZ EM C ##\n");
@@ -17,22 +18,53 @@ int main(){
     {
     case 1:
         printf("Você tem Rei, Bispo e torre, Qual das peças deseja seguir");
-        printf("1. Rei\n");
-        printf("2. Bispo\n");
-        printf("3. Torre\n");
-        scanf("%d", decisao2);
+        printf("R. Rei\n");
+        printf("B. Bispo\n");
+        printf("T. Torre\n");
+        printf("C. Cavalo\n");
+        scanf(" %c", &decisao2);
 
         switch (decisao2)
         {
-        case 'Rei':
-        case 'rei':
+        case 'R':
+        case 'r':
             printf("Você escolheu Rei\n");
-            break;
-        case 'Bispo':
-        case 'bispo':
-            printf("Você escolheu Bispo\n");
 
+            i = 0;
+
+            do {
+            printf("Esquerda\n");
+            i++;
+            } while(i < 8);
+            
+            break;
+        case 'B':
+            printf("Você escolheu Bispo\n");
+            i = 0;
+
+            while(i < 5) {
+            printf("Cima, Direita\n");
+            i++;
+}
+            break;
+        case 'T':
+            printf("Você escolheu Torre!\n");
+            for(i = 0; i < 5; i++) {
+             printf("Direita\n");
+             }
+            break;
+            case 'C':
+            printf("Você escolheu Cavalo\n");
+            int esquerda = 1;
+            while(esquerda--){
+                for(int cima = 0; cima < 2; cima++){
+                    printf("Cima\n");
+                }
+                printf("Direita\n");
+            }
+            break;
         default:
+            printf("Opção invalida!\n");
             break;
         }
         break;
